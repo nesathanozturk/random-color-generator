02acf9d748d1757510f7189d1a05ce680a086778
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-24 22:45:46 +0300

    feat: added functions

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const bodyColor = document.getElementById("color");
+const btn = document.querySelectorById("btn");
+
+function getRandomColor() {
+  let characters = "0123456789ABCDEF";
+  let hex = "";
+
+  for (let i = 0, c = characters.length; i < 6; i++) {
+    hex += characters.charAt(Math.floor(Math.random() * c));
+  }
+
+  bodyColor.innerText = `Color Hex: #${hex}`;
+  document.querySelector("body").style.backgroundColor = `#${hex}`;
+}
